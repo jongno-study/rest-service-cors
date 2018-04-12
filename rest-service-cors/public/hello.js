682ef51090086c4d1aec6b27d29f1c8db7a7bee4
chori <slivers721@gmail.com>
2018-04-13 00:20:14 +0900

    Enabling Cross Origin Requests for a RESTful Web Service #4
    - WebMvcConfigurer를 이용한 Global CORS 설정 추가

--- rest-service-cors/public/hello.js
+++ rest-service-cors/public/hello.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  $.ajax({
-    url: "http://localhost:8080/greeting"
-  }).then(function(data, status, jqxhr) {
-    $('.greeting-id').append(data.id);
-    $('.greeting-content').append(data.content);
-    console.log(jqxhr);
-  });
+    $.ajax({
+        url: "http://localhost:8080/greeting"
+    }).then(function(data, status, jqxhr) {
+        $('.greeting-id').append(data.id);
+        $('.greeting-content').append(data.content);
+        console.log(jqxhr);
+    });
+				
+				$.ajax({
+								url: "http://localhost:8080/hello"
+				}).then(function(data, status, jqxhr) {
+        $('.hello').append(data);
+								console.log(jqxhr);
+				});
 });
